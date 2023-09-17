d6a3c09fce4f84885ac0997275fe67edd3db8fcf
shinyeongwoon <duddns119@naver.com>
2023-09-17 12:09:55 +0900

    GIT FIRST COMMIT
    first commit from shinyeongwoon

---
+++ back/src/main/java/ten/give/GiveApplication.java
@@ -0,0 +1,13 @@
+package ten.give;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/ten/give/GiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ten.give;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
